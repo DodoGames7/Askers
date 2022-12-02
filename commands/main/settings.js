b5433424dbdff52721677e4c803fb7895635264f
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-12-02 19:26:04 +0200

    add "Language" button

--- commands/main/settings.js
+++ commands/main/settings.js
@@ -13,6 +13,7 @@ module.exports = {
   Button: \`$getServerVar[akin_buttons]\`
   Mode: \`$getServerVar[akin_gametype]\`
   ]
+  $addButton[1;Language;primary;language_$authorID;no]
   $addButton[1;Mode;primary;mode_$authorID;no]
   $addButton[1;Button;primary;button_$authorID;no]
   $color[YELLOW]
