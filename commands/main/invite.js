8f6a98a298a780cca95f34f3a5d01bfff8a417ff
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-12-07 17:39:08 +0200

    use buttons (finally)

--- commands/main/invite.js
+++ commands/main/invite.js
@@ -1,6 +1,7 @@
 module.exports = {
   name: "invite",
   code: `$title[inviting Askers]
-  $description[you can invite Askers to your discord server with this [link]($getBotInvite[sendmessage;embedlinks;readmessagehistory;viewchannel])
+  $description[you can invite Askers to your discord server by clicking on the button Below]
+  $addButton[1;Invite;link;$nonEscape[$getBotInvite[sendmessage;embedlinks;readmessagehistory;viewchannel]]
   ]`
 }
