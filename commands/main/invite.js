bd6163d287607d2e1ac05ce9292df9447299aaa8
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-12-07 17:58:31 +0200

    fixed smth

--- commands/main/invite.js
+++ commands/main/invite.js
@@ -2,6 +2,6 @@ module.exports = {
   name: "invite",
   code: `$title[inviting Askers]
   $description[you can invite Askers to your discord server by clicking on the button Below]
-  $addButton[1;Invite;link;$nonEscape[$getBotInvite[sendmessage;embedlinks;readmessagehistory;viewchannel]]
+  $addButton[1;Invite;link;$nonEscape[$getBotInvite[sendmessage;embedlinks;readmessagehistory;viewchannel]];no]
   ]`
 }
