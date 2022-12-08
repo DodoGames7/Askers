8991fca9a60924ee03f3fc41816a74074e8288a6
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-12-08 21:25:35 +0200

    copyright, nice

---
+++ commands/main/credits.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "credits",
+  code: `$title[Asker's Credits]
+  $description[all of the code of the bot belongs to $userTag[632607624742961153] you shouldn't take a code from here without giving credits so be aware!]
+ $footer[@2019 dodoGames development]
+ $color[YELLOW]
+ `
+}
