0f352d7deddb01d79ec494cec0973ebc9edaff1e
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-12-17 14:51:35 +0200

    add an button leading to source code link

--- commands/main/credits.js
+++ commands/main/credits.js
@@ -4,5 +4,6 @@ module.exports = {
   $description[all of the code of the bot belongs to $userTag[632607624742961153] you shouldn't take a code from here without giving credits so be aware!]
  $footer[@2019 dodoGames development]
  $color[YELLOW]
+ $addButton[1;Source Code;link;https://github.com/DodoGames7/Askers;no]
  `
 }
