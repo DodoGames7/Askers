05ed3410462cfcb39133ac15bb07bd59da6d023c
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-12-17 20:53:21 +0200

    k

--- commands/main/ver.js
+++ commands/main/ver.js
@@ -3,7 +3,7 @@ module.exports = {
   aliases: "ver",
   code: `$title[1.0.0-beta1, minor fix]
   $description[**changes**
-the user should now get permissions error in case if it's missing when attempting to start the game
+the user should now get error in case if a permission are missing when attempting to start the game
 added credits command including an button to redirect to the source code
 
 add alias \`ver\` for \`version\` command
