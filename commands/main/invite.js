aff4bea9232f6e10021a24ec84377ec53391d807
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-12-21 18:11:41 +0200

    "Other Projects" Button

--- commands/main/invite.js
+++ commands/main/invite.js
@@ -2,6 +2,7 @@ module.exports = {
   name: "invite",
   code: `$title[inviting Askers]
   $description[you can invite Askers to your discord server by clicking on the button Below]
+  $addButton[1;Other Projects;primary;project;no;🤖]
   $addButton[1;Invite;link;$nonEscape[$getBotInvite[sendmessage;viewchannel;managemessages]];no]
   `
 }
