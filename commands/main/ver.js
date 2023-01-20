a2ae67dda1e3163c9e5bc10ff3810cdff5ab18d3
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-01-20 16:31:14 +0200

    Update ver.js

--- commands/main/ver.js
+++ commands/main/ver.js
@@ -5,6 +5,7 @@ module.exports = {
   $description[
     **changes**
    Askers is now using aoi.js v6 since v5 will began to break gradually
+   buttons has been enabled by default, you can still revert to text mode by going into settings and then click on "Button"
 
    **language system is now Available**
    it supports more than English and Arabic, it supports so many languages such as polish, german, russian and etc! check out
