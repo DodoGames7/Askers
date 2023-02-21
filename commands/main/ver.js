1bac6e41d973c3383309235551329f7fadebabd1
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-02-21 14:35:15 +0200

    1.0.2-patched

--- commands/main/ver.js
+++ commands/main/ver.js
@@ -1,27 +1,13 @@
 module.exports = {
   name: "version",
   aliases: "ver",
-  code: `$title[1.0.2-stable]
+  code: `$title[1.0.2-patched]
   $description[
-    **changes**
-  updated to 6.1.0 version of v6 (github version)
-  buttons are now enabled by default (you can still disable them through \`settings\` cmd though.)
-  add alias \`akinator\` to \`start\` cmd
-
-
-**new languages**
-\`japanese\`
-\`Spanish\`
-\`Hindi\`
-\`Bengali\`
-note: keep in mind that these are not the very first languages to be added since we have added previous languages such as Russian before go explore them within \`settings\` cmd
-
-**previous changes**
-updated Askers to support aoi.js v6! the reason for this change is that v5 is subjected to get broken in the upcoming months despite being no longer maintained by official devs
-akinator language system has been officially released
-jumped from 1.0.1-alpha to 1.0.2-alpha and cancel \`1.0.1\` the alpha branch will be updated someday
+    **fixes**
+    fixed the bot being able to respond to other bots if it has commands triggered (this was a issue on aoi.js so you can most likely say that it was a bug from it)
+    fixed \`update\` command for bots devs being not actually reloading commands
   ]
   $color[Yellow]
-  $footer[winner winner chicken dinner!]
+  $footer[there we go again!]
 `
 }
