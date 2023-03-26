e8fd4ebe8edc76e2f3ee9497d32181065b7f8607
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-03-26 19:30:59 +0200

    add function command

---
+++ commands/owner/func.js
@@ -0,0 +1,23 @@
+module.exports = [{
+    name: "func",
+    aliases: "aoi",
+    info: {
+      description: "look for information about aoi.js functions",
+      usage: "a!func < function name >",
+      notes: "user must be the bot owner to use the command"
+    },
+    code: `
+    $author[$userTag[$authorID];$userAvatar[$authorID]]
+    $title[$advancedTextSplit[$getObjectProperty[usage];[;1]]
+    $addField[Example;$getObjectProperty[example]]
+    $addField[Usage;\`$replaceText[$getObjectProperty[usage];\r;]\`]
+    $addField[Description;$getObjectProperty[desc]]
+    $color[White]
+    $addTimestamp
+    $addButton[1;Source Code;link;$nonEscape[$getObjectProperty[src]];false]
+    $addButton[1;Documentation;link;$nonEscape[$getObjectProperty[link]];false]
+    $onlyIf[$getObjectProperty[err]!=DOCS NOT FOUND;]
+    $createObject[$jsonRequest[https://aoijs-api.bumblebeerox1.repl.co/api/aoijs/function?name=$message]]
+    $onlyIf[$checkContains[$message[1];$]==true;]
+    $onlyForIDS[$botOwnerID;]`
+}] // disclaimer: api has credits
