9ebb85ceedac3d2b196cab492c9e99e8737a173a
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-04-13 14:36:00 +0000

    k

--- commands/interactions/color.js
+++ commands/interactions/color.js
@@ -1,7 +1,7 @@
 module.exports = [{
   type: "interaction",
   prototype: "button",
-  code: `$interactionReply[choose a color from the list.;;{actionRow:{selectMenu:colormenu:Color Selection:1:1:false:{selectMenuOptions:Default:default:default used color:false}{selectMenuOptions:Red:red:set color to Red:false}{selectMenuOptions:Blue:blue:set color to Blue:false}{selectMenuOptions:Green:green:set color to Green:false}{selectMenuOptions:Orange:orange:set color to Orange:false}{selectMenuOptions:Black:black:set color to Black:false}{selectMenuOptions:White:White:set color to White:false}{selectMenuOptions:Gold:gold:definitely something rich people would use:false}{selectMenuOptions:Old Blurple:oldblurple:discord's original blurple:false}{selectMenuOptions:Blurple:blurple:discord's default branding color:false}{selectMenuOptions:Luminous Vivid Pink:LuminousVividPink:pink color but a bit different:false}{selectMenuOptions:invisible:invisible:makes embed color invisible (works only on Dark Mode.):false}{selectMenuOptions:Rainbow:rainbow:makes embed color randomly change in each game:false}{selectMenuOptions:Fuchsia:pink:set color to pink:false}}};;all;true]
+  code: `$interactionReply[choose a color from the list.;;{actionRow:{selectMenu:colormenu:Color Selection:1:1:false:{selectMenuOptions:Default:default:default used color:false}{selectMenuOptions:Red:red:set color to Red:false}{selectMenuOptions:Blue:blue:set color to Blue:false}{selectMenuOptions:Green:green:set color to Green:false}{selectMenuOptions:Orange:orange:set color to Orange:false}{selectMenuOptions:Black:black:set color to Black:false}{selectMenuOptions:White:White:set color to White:false}{selectMenuOptions:Gold:gold:definitely something rich people would use:false}{selectMenuOptions:Old Blurple:oldblurple:discord's original blurple:false}{selectMenuOptions:Blurple:blurple:discord's default branding color:false}{selectMenuOptions:Luminous Vivid Pink:LuminousVividPink:pink color but a bit different:false}{selectMenuOptions:invisible:invisible:makes embed color invisible (works only on Dark Mode.):false}{selectMenuOptions:Rainbow:rainbow:makes embed color randomly change in each game:false}{selectMenuOptions:Fuchsia:pink:set color to pink:false}{selectMenuOptions:Custom:custom:use your own color. (Experimental):false}}};;all;true]
 
   $onlyif[$get[authorID]==$interactionData[author.id];
   You aren't the author of this interaction.
@@ -210,4 +210,40 @@ that's already being used
 
 $onlyIf[$interactionData[values[0]]==oldblurple;]
 `
+},{
+    name: "colormenu",
+    type: "interaction",
+    prototype: "selectMenu",
+    code: `$interactionModal[set your own Color!;customcolormenu;
+        {actionRow:
+          {textInput:hex code?:1:hexInput:true:e.g #FF0000:7:7}
+        }
+
+$onlyIf[$interactionData[values[0]]==custom;]
+`
+},{
+    name: "customcolormenu",
+    type: "interaction",
+    prototype: "modal",
+    code: `$setGuildVar[akin_embedcolor;$textInputValue[hexInput]]
+    $interactionReply[changed color to hex code \`$textInputValue[hexInput]\`;;;;all;true]
+    
+    $onlyIf[$checkContains[$textInputValue[hexInput];#]==true;your hex code must contain the \`#\` before you can insert hex code.
+    {options:{ephemeral: true}}
+    {extraOptions:{interaction: true}}
+    ]
+    
+    $onlyIf[$isValidHex[$textInputValue[hexInput]]==true;your hex code is invalid, please try to insert an valid hex code.
+    {options:{ephemeral: true}}
+    {extraOptions:{interaction: true}}
+    ]
+
+    $onlyIf[$getGuildVar[akin_embedcolor]!=$textInputValue[hexInput];
+that's already being used
+{options:{ephemeral: true}}
+{extraOptions:{interaction: true}}
+]
+
+
+    `
 }]
