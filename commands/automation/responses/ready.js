49f0f2c5ffcf4e24918b224a52ecf4bfb57c60d9
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-04-15 19:11:46 +0000

    goodbye `from`

--- commands/automation/responses/ready.js
+++ commands/automation/responses/ready.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "readycommand",
   channel: "",
   type: "ready",
-  code: `$log[Ready!, invite the bot from here: $getBotInvite[sendmessages;viewchannel;managemessages]
+  code: `$log[Ready!, invite the bot here: $getBotInvite[sendmessages;viewchannel;managemessages]
   WARNING: you're using an beta build of Askers which means that new changes or features may be unstable, be careful]
 `
 }
