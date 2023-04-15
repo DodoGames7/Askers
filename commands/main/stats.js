39109866d02878cee131f5bbe3bca8f1e0d82207
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-04-15 09:19:15 +0000

    Library Language -> Bot Lang

--- commands/main/stats.js
+++ commands/main/stats.js
@@ -8,7 +8,7 @@ module.exports = {
   code: `$title[Askers's stats]
   $addField[Uptime;$uptime[humanize]]
   $addField[Askers version;1.0.3-beta2]
-  $addField[Library Language;Aoi.js;true]
+  $addField[Bot Lang;[Aoi.js](https://www.npmjs.com/package/aoi.js);true]
   $addField[Amount of ram;$round[$ram]%;true]
   $addField[Amount of CPU;$cpu;true]
   $addField[node.js;$nodeVersion;true]
