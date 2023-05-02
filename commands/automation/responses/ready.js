138815cf4034fa04842978a2c8d98bd68014c70a
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-05-02 09:37:30 +0300

    fix undefined error

--- commands/automation/responses/ready.js
+++ commands/automation/responses/ready.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "InviteLogger",
   channel: "",
   type: "ready",
-  code: `$log[Ready! Invite the bot here: $getBotInvite[sendmessages;viewchannel;managemessages]
+  code: `$log[Ready! Invite the bot here: $getClientInvite[sendmessages;viewchannel;managemessages]
   WARNING: you're using an Preview build of Askers. Stuff like features in this version are subject to change before the final release.]
 `
 }
