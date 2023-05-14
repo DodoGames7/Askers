3be2c392a7b9208229c199c6a3e56865f24dc16b
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-05-14 15:10:04 +0300

    removed preview warning

--- commands/automation/responses/ready.js
+++ commands/automation/responses/ready.js
@@ -3,6 +3,6 @@ module.exports = {
   channel: "",
   type: "ready",
   code: `$log[Ready! Invite the bot here: $getClientInvite[sendmessages;viewchannel;managemessages]
-  WARNING: you're using an Preview build of Askers. Stuff like features in this version are subject to change before the final release.]
+  WARNING: you're using an Alpha build of Askers. This version is subject to have bugs. Use it at your own risk!]
 `
 }
